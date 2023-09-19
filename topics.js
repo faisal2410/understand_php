ddd7a6d7e4a291fe57931ca275351af0bed9d7be
Faisal ahmed <faisal2410@yahoo.com>
2023-09-19 10:40:01 +0600

    updated

---
+++ topics.js
@@ -0,0 +1,291 @@
+// Module No	Topics	Subtopics
+// 1	Foundations of PHP and Laravel Web Development: Setup, Syntax, and Efficient Coding Practices	PHP Environment Setup
+// 		Variable
+// 		Constant
+// 		Comments
+// 		Printing output
+// 		Addition Subtraction Multiplication Division in PHP
+// 		Discussion of different number systems
+// 		printf
+// 		sprintf
+// 		Conditional Operators
+// 		Logical Operators
+// 		If Else
+// 		Nested If Else
+// 		Ternary Operators
+// 		Nested Ternary Operators
+// 		switch case
+// 		VSCode Power Tips
+// 		VSCode Settings & Extensions for PHP Laravel Web Development
+		
+// 2	Mastering Loops, Functions, and Recursion: Building Efficient PHP and Laravel Applications.	Different types of loops
+// 		Multiple Stepping in For Loops
+// 		Continue and break between loops
+// 		Printing the Fibonacci Series with Loops
+// 		Declaring Functions
+// 		Executing function
+// 		parameter
+// 		argument
+// 		default parameter
+// 		Type hinting
+// 		Function Return Type
+// 		Accepting Unlimited Arguments in Functions
+// 		Dividing a large function into smaller functions
+// 		Recursion and Recursive Functions
+// 		Printing Fibonacci Series Using Recursive Functions
+// 		Variable Scope in PHP Function
+		
+// 3	Mastering Arrays and String Manipulation in PHP and Laravel: Techniques for Efficient Data Handling.	Array Declaration
+// 		Array Manipulation
+// 		Detailed discussion of associative arrays
+// 		Converting String to Array
+// 		Converting Array to String
+// 		Multiple Delimiters
+// 		Multidimensional or nested arrays
+// 		Associative Array to String Conversion - Serialize
+// 		json
+// 		Removing data from associative arrays
+// 		Discussion of Empty Values
+// 		Decluttering an array
+// 		Concatenating
+// 		Sorting
+// 		Indexing
+// 		Searching
+// 		Array Utility Functions Walk
+// 		Map
+// 		Filter
+// 		Reduce
+// 		shuffling
+// 		ASCII Code
+// 		Accessing characters within a string
+// 		String Reversing
+// 		Breaking
+// 		Searching
+// 		trimming
+// 		replacing
+// 		WordWrap ​​
+		
+// 4	Mastering Object-Oriented PHP and Laravel: Building Efficient and Scalable Web Applications with OOP Principles.	Object-Oriented Programming Structure
+// 		Classes
+// 		Objects
+// 		Methods
+// 		Properties
+// 		Access modifiers
+// 		Public, Private,protected, static Methods and Properties of Classes
+// 		Writing a Real-Life Useful Class
+// 		Inheritance
+// 		Abstract
+// 		Static
+// 		Final
+// 		Better code with OOP by forcing
+// 		Interfaces & Its practical Example
+// 		Magic Methods
+// 		Object Cloning
+// 		Conversion
+// 		Comparing
+// 		Detailed discussion on early and late binding
+// 		Namespaces
+// 		Method Overloading and Overriding
+		
+// 5	Efficient Data Management and Error Handling in PHP and Laravel Web Applications: Files, Sessions, and Debugging Techniques.	File Read, Write, Delete, Append
+// 		Processing data in files using serialized arrays
+// 		JSON format
+// 		Creating Sessions and Working with Session Data
+// 		Simple User Authentication with Sessions
+// 		Basic Discussion of Hashes
+// 		Basic Role Management (Access Control)
+// 		Details on cookies & session storage
+// 		PHP error settings and error reporting
+// 		PHP Error Log
+// 		Custom Error Handlers
+// 		Fatal Error Handlers
+// 		Discussion of error logs
+// 		Backtracking for Debugging
+		
+// 6	Mastering Database Management and SQL in PHP and Laravel: Building Robust Data-Driven Web Applications	Details on Database
+// 		Tables
+// 		Columns
+// 		Properties
+// 		SQL Query writing select, delete, insert, update
+// 		Grouping
+// 		Sorting
+// 		Limiting
+// 		Union
+// 		SQL Aggregator Functions
+// 		SQL inner join
+// 		left join
+// 		right join
+// 		One-to-One
+// 		One-to-Many (1:N)
+// 		Only-One-to-Many (1:N)
+// 		Many-to-Many (N:M)
+// 		Self-Referencing Relationship
+// 		SQL best practices
+		
+// 7	Laravel Mastery: Navigating Framework Fundamentals, Routing, Controllers, and Client-Server Interaction for Dynamic Web Development	Laravel Structure
+// 		Artisan Commands
+// 		Laravel Routing Structure
+// 		Start Using Postman Server-Side Simulation
+// 		Request-Response Model
+// 		Server Side Client Side
+// 		Deep inside Routing Middleware
+// 		Controller Flow in Details
+// 		JSON Structure & Best Practices
+// 		Middleware
+// 		Controller
+// 		Request
+// 		Response
+// 		Understanding HTTP Client In details
+// 		URI Naming Best Practices
+// 		HTTP Cookies in details
+// 		Encryption Decryption Process
+// 		JWT Structure & Concept
+// 		JWT Encryption Decryption Simulate
+// 		Client side rendering
+// 		Server side rendering
+		
+// 8	Efficient Request Handling and Dynamic Responses in Laravel: Navigating User Input, APIs, and Data Presentation.	Request
+// 		Parameter
+// 		JSON Body
+// 		Request Header
+// 		Working With Form Data & File Upload
+// 		Request IP Address And Content
+// 		Working With Request Cookie
+// 		Different Response Format
+// 		Response JSON
+// 		Redirect Response
+// 		Binary And File Download Response
+// 		Cookie Response
+// 		Response Header
+// 		Response View
+		
+// 9	Advanced Laravel Techniques: Middleware, Controllers, Logging, and Blade Templates for Effective Web Application Development.	Middleware Structure
+// 		Request Verification
+// 		Request Redirect
+// 		Middleware Apply For Route And Route Group
+// 		For Whole Application
+// 		Middleware Manipulate Request Header
+// 		Rate Limiting Middleware
+// 		Controller Concept In Details
+// 		Single Action Controller
+// 		Resource Controller
+// 		Controller Constructor
+// 		Laravel Log Feature
+// 		Session Set Get, Clear
+// 		Blade Template Engine & Attributes
+// 		Blade Directives
+// 		@if-else
+// 		@switch-case
+// 		@for
+// 		@while
+// 		@foreach
+// 		master layout
+// 		@extend
+// 		@include
+// 		@yield
+// 		@content
+// 		Blade data passing
+// 		Asset binding
+// 		Including blade subview
+		
+// 10	Database Design and Relationships in Laravel: Mastering Migrations, Constraints, and Entity-Relationship Diagrams for Effective Data Structures.	Exploring Migrations
+// 		Naming conventions
+// 		Migration Structure
+// 		Database columns types in migration
+// 		Working with different columns types and attributes
+// 		Table Column Modification Add, Rename, Drop
+// 		Details on Database Relationship
+// 		One-to-One (1:1)
+// 		One-to-Many (1:N)
+// 		Only-One-to-Many (1:N),
+// 		Many-to-Many (N:M)
+// 		Self-Referencing Relationship
+// 		Details on ERD Diagram
+// 		Details on Relationship Constraint - RESTRICT, CASCADE
+		
+// 11	Laravel Query Building Mastery: Constructing Powerful Database Queries and Advanced Data Manipulation Techniques for Web Development	Why and how Query Builder works
+// 		Retrieving All Rows
+// 		Single Rows
+// 		Aggregates
+// 		Select Clauses
+// 		Where Clauses
+// 		Advance Clauses
+// 		Inner Join
+// 		Left Right
+// 		Right Join
+// 		Cross Join
+// 		Advanced Join Clauses
+// 		Unions
+// 		Order
+// 		Latest
+// 		Oldest
+// 		Skip
+// 		Take
+// 		Limit
+// 		Group By And Having
+// 		Insert
+// 		Update
+// 		Detele
+// 		Upsert
+// 		Pagination
+// 		Custom Pagination
+		
+// 12	Eloquent ORM Deep Dive: Mastering Model Relationships, CRUD Operations, and Advanced Data Management for Laravel Applications.	Why and how Eloquent ORM works
+// 		Model Naming Conventions
+// 		Insert
+// 		Update
+// 		Delete
+// 		Fillable Properties
+// 		Increment Decrement
+// 		Retrieving Rows Columns
+// 		Aggregation
+// 		Select Clause
+// 		Where Clause
+// 		Advance Clause
+// 		Ordering
+// 		Grouping
+// 		Limiting
+// 		Pagination
+// 		Custom Pagination
+// 		One-to-One (1:1)
+// 		One-to-Many (1:N)
+// 		Only-One-to-Many (1:N)
+// 		Many-to-Many (N:M)
+// 		Self-Referencing Relationship
+// 		Details On Database & Model Designing
+		
+// 13	Building Dynamic Multiuser Applications with Laravel: Agile Workflow, Modern Ajax, JWT Authentication, and Profile Management	Working process on the agile method
+// 		Laravel Modern Ajax(Axios) Project Architecture
+// 		Multiuser application structure
+// 		CMS structure
+// 		JWT Auth
+// 		Token Encode-Decode
+// 		Store, Login
+// 		Registration
+// 		Forget Password
+// 		Reset Password
+// 		Multiuser application profile management
+		
+// 14	End-to-End Inventory Management System Development: From Categories and Products to Sales, Invoicing, Reporting, and Deployment with Laravel.	Inventory Category Management (Database, Back-End, Front-End)
+// 		Inventory Product Management (Database, Back-End, Front-End)
+// 		Inventory Sales Management (Database, Back-End, Front-End)
+		
+// 		Inventory Invoice & Printing (Database, Back-End, Front-End)
+// 		Inventory Report & Dashboard Summary Generation (Database, Back-End, Front-End)
+// 		Application Testing & Live Server Deploy
+		
+// 15	Building Robust E-Commerce Solutions: Payment Gateway Integration, Database Design, and Complete Back-End Development with Laravel.	SSL Commerz & AamarPay Call Flow Diagram Desing
+// 		Database design for Payment getaway
+// 		Payment gateway success, cancel, fail, verification managing
+// 		Payment IPN(Instant payment notification) Development
+// 		E-commerce Database design
+// 		E-commerce Complete back-end development
+		
+// 16	Creating Dynamic E-Commerce Experiences: User Authentication, Product Management, Cart Functionality, and Secure Checkout with Laravel	Product Brand, Category, Latest Product
+// 		Popular product, Featured, Slider, Product Details
+// 		User Login Using OTP & Profile
+// 		Product Cart List management
+// 		Wish List management
+// 		Product User Review & Rating System
+// 		Cart Checkout using SSL Commerz payment
+// 		Order history & Invoice generation
